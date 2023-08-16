27da2a3c61837aebfca08718060566877d15b96b
CHORU-OFFICIAL <112081055+ChoruTiktokers182@users.noreply.github.com>
2023-08-17 01:09:57 +0800

    Update gptgo.js

--- gptgo.js
+++ gptgo.js
@@ -4,41 +4,48 @@ const fetch = require('node-fetch');
 
 router.get("/", async (req, res, next) => {
   try {
-    const query = req.query.q; // Get query parameter from request
+    const query = req.query.q; 
+    if (!query) {
+      return res.status(400).send("Query parameter 'q' is required");
+    }
     const response = await ChatGpt(query);
     res.send(response);
   } catch (error) {
-    res.status(500).send(error.message);
+    res.status(500).send("Internal Server Error: " + error.message);
   }
 });
 
 async function ChatGpt(query) {
-  try {
-    const tokenResponse = await fetch(`https://gptgo.ai/action_get_token.php?q=${encodeURIComponent(query)}&hlgpt=default`, {
-      method: "GET",
-      headers: {
-        "Referer": "https://gptgo.ai/?hl=id",
-        "origin": "https://gptgo.ai/",
-      }
-    });
-    
-    const tokenData = await tokenResponse.json();
-    const gpttoken = tokenData.token;
+  const tokenResponse = await fetch(`https://gptgo.ai/action_get_token.php?q=${encodeURIComponent(query)}&hlgpt=default`, {
+    method: "GET",
+    headers: {
+      "Referer": "https://gptgo.ai/?hl=id",
+      "origin": "https://gptgo.ai/",
+    }
+  });
 
-    const response = await fetch(`https://gptgo.ai/action_ai_gpt.php?token=${gpttoken}`, {
-      method: "GET",
-      headers: {
-        "Referer": "https://gptgo.ai/?hl=id",
-        "origin": "https://gptgo.ai/",
-        "accept": "text/event-stream"
-      }
-    });
-
-    const { content } = await response.json();
-    return content; // Return the content from the response
-  } catch (error) {
-    throw new Error("Error fetching from GPT: " + error.message);
+  if (!tokenResponse.ok) {
+    throw new Error(`Failed to fetch token. Status: ${tokenResponse.status}`);
   }
+
+  const tokenData = await tokenResponse.json();
+  const gpttoken = tokenData.token;
+
+  const response = await fetch(`https://gptgo.ai/action_ai_gpt.php?token=${gpttoken}`, {
+    method: "GET",
+    headers: {
+      "Referer": "https://gptgo.ai/?hl=id",
+      "origin": "https://gptgo.ai/",
+      "accept": "text/event-stream"
+    }
+  });
+
+  if (!response.ok) {
+    throw new Error(`Failed to fetch GPT response. Status: ${response.status}`);
+  }
+
+  const { content } = await response.json();
+  return content;
 }
 
 module.exports = router;
